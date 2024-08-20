388733cb8f901fc4b4b5e8a99da312dd9cf82812
Muh Haikal Akbar <haikalakbar10k@gmail.com>
2024-08-20 13:07:23 +0800

    Push 1 Test

--- Learn How to Work with Numbers and Strings by Implementing the Luhn Algorithm/main.py
+++ Learn How to Work with Numbers and Strings by Implementing the Luhn Algorithm/main.py
@@ -3,3 +3,4 @@ print('Hai My Name is Muh Haikal Akbar')
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
+    translated_card_number = card_number.translate(card_translation)
